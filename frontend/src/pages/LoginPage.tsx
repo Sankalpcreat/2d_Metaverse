5b467c68740a69ce96f82e09c0da843b6b5271a8
Sankalp Singh <singhsankalp997@gmail.com>
2024-10-26 21:49:07 +0530

    login page basic code

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,1 +1,13 @@
-// View for login page
+import React from 'react';
+import Login from '../auth/Login'; 
+
+const LoginView: React.FC = () => {
+  return (
+    <div>
+      <h2>Login to Your Account</h2>
+      <Login />
+    </div>
+  );
+};
+
+export default LoginView;
