4b7b2816a9e73b02defd252f26717bc8e791defa
Sankalp Singh <singhsankalp997@gmail.com>
2024-10-26 21:39:57 +0530

    vite config changes

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@assets': path.resolve(__dirname, './public/assets'),  
+      '@components': path.resolve(__dirname, './src/components') 
+    }
+  },
+  assetsInclude: ['**/*.png', '**/*.json'],  
+
+  build: {
+    outDir: 'dist',  
+    sourcemap: true   
+  }
+});
