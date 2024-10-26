4234a87b489711001a793f0996d538ef57c09445
Sankalp Singh <singhsankalp997@gmail.com>
2024-10-26 21:48:22 +0530

    Landing page just basic routing setup

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,1 +1,21 @@
-// Home view (intro, options to login/join rooms, etc.)
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <h1>Welcome to the Game</h1>
+      <p>Select an option to continue:</p>
+      <div>
+        <Link to="/login">
+          <button>Login</button>
+        </Link>
+        <Link to="/signup">
+          <button>Sign Up</button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
