fb51731ca11a6962fb147b85347994027c186e45
Sankalp Singh <singhsankalp997@gmail.com>
2024-10-26 21:52:56 +0530

    game room basic setup

--- frontend/src/pages/GameRoom.tsx
+++ frontend/src/pages/GameRoom.tsx
@@ -1,1 +1,17 @@
-// Main game room where the map and player interactions are rendered
+import React from 'react';
+import Map from '../components/Map'; 
+import Avatar from '../components/Avatar'; 
+import UI from '../components/UI';
+
+const GameRoom:React.FC=()=>{
+    return (
+        <div>
+            <h2>Welcome to 2d-Metaverse</h2>
+            {/* here we will incluedes thigns related to phasor  */}
+            <Map/>
+            <Avatar/>
+            <UI/>
+        </div>
+    )
+}
+export default GameRoom
