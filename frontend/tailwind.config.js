0e0b29276cffa356b6b35b719dcd15201fa67715
Parikshit <parikshitgupta2001@gmail.com>
2024-10-27 17:06:25 +0530

    Add tailwind

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    colors: {
+      purple: "rgb(51, 58, 100)",
+      dark_purple: "rgb(40, 45, 78)",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
