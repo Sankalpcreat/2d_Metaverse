50fb5e49b947a4a46c65b8be347ea22e99a2733c
Sankalp Singh <singhsankalp997@gmail.com>
2024-10-27 20:49:48 +0530

    phaser preload game scene

--- frontend/src/phaser/PreloadScene.ts
+++ frontend/src/phaser/PreloadScene.ts
@@ -1,1 +1,20 @@
-// Scene for preloading all assets (tilemap, sprites)
+import Phaser from "phaser";
+
+export class PreloadScene extends Phaser.Scene{
+    constructor(){
+        super({key:'PreloadScene'});
+    }
+    preload(){
+        // Load the tilemap JSON and the tileset image
+        this.load.tilemapTiledJSON('map', 'assets/ss1.json');
+
+        this.load.image('tiles', 'assets/ss1.png');
+        
+ // You can load other assets, plyaer avatar
+        this.load.spritesheet('player','assets/avatar.png',{frameWidth:32,frameHeight:32})
+    }
+    create() {
+        // Once assets are loaded, start the game scene
+        this.scene.start('GameScene');
+    }
+}
