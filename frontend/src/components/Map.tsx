eb7d72696f54460d45ad986786ada7b29650b463
Sankalp Singh <singhsankalp997@gmail.com>
2024-10-28 12:04:45 +0530

    map for components

--- frontend/src/components/Map.tsx
+++ frontend/src/components/Map.tsx
@@ -1,17 +1,20 @@
 import React, { useEffect } from 'react';
 import Phaser from 'phaser';
-import { useDispatch } from 'react-redux';
-import { updatePosition } from '../store/playerSlice';
+// PreloadScene to load assets like tilesets and the GameScene
+import { PreloadScene } from '../phaser/PreloadScene';
 
-const Map: React.FC = () => {
-  const dispatch = useDispatch();
+import { GameScene } from '../phaser/GameScene';
+
+export const Map: React.FC = () => {
+  // This sets up the game world with dimensions, physics, and scenes.
 
   useEffect(() => {
-
     const config: Phaser.Types.Core.GameConfig = {
       type: Phaser.AUTO,
       width: 800,
       height: 600,
+      parent: 'phaser-game',
+      scene: [PreloadScene, GameScene],
       physics: {
         default: 'arcade',
         arcade: {
@@ -19,51 +22,14 @@ const Map: React.FC = () => {
           debug: false,
         },
       },
-      scene: {
-        preload: preload,
-        create: create,
-        update: update,
-      },
     };
 
-    const game = new Phaser.Game(config);
-
-    function preload(this: Phaser.Scene) {
-      
-      this.load.tilemapTiledJSON('tilemap', 'assets/ss1.json');
-      this.load.image('tiles', 'assets/ss1.png');
-    }
-
-    function create(this: Phaser.Scene) {
-    
-      const map = this.make.tilemap({ key: 'tilemap' });
-      const tileset = map.addTilesetImage('TX Struct', 'tiles');
-      map.createLayer('Tile Layer 1', tileset, 0, 0);
-
-      const player = this.physics.add.sprite(100, 100, 'avatar');
-      this.physics.world.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
-      this.cameras.main.startFollow(player);
-    }
-
-    function update(this: Phaser.Scene) {
-    
-      const cursors = this.input.keyboard.createCursorKeys();
-      const player = this.physics.world.bounds;
-      if (cursors.left.isDown) {
-        dispatch(updatePosition({ x: player.x - 10, y: player.y }));
-      }
-      if (cursors.right.isDown) {
-        dispatch(updatePosition({ x: player.x + 10, y: player.y }));
-      }
-     
-    }
+    new Phaser.Game(config); // Initialize the Phaser game instance
 
     return () => {
-      game.destroy(true);
+      Phaser.Game.destroy(true); // Cleanup after finished
     };
-  }, [dispatch]);
+  }, []);
 
-  return <div id="phaser-game"></div>;
+  return <div id="phaser-game" style={{ width: '800px', height: '600px' }} />;
 };
-
-export default Map;
