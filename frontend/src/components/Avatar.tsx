fa09f7508b8a4754583297f4c658a7ebbe8af28d
Sankalp Singh <singhsankalp997@gmail.com>
2024-10-28 12:13:55 +0530

    changes in avatar of components

--- frontend/src/components/Avatar.tsx
+++ frontend/src/components/Avatar.tsx
@@ -1,40 +1,88 @@
-import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import React, { useEffect } from 'react';
+import Phaser from 'phaser';
+// useDispatch and useSelector are used to manage player data (like the position) and update the global state via Redux.
+import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '../store';
+// The player’s position is continuously updated in the Redux store using the updatePosition action from the playerSlice.ts.
 import { updatePosition } from '../store/playerSlice';
+import { Player } from '../phaser/Player';
 
-const Avatar: React.FC = () => {
-  const player = useSelector((state: RootState) => state.player);
+export const Avatar: React.FC = () => {
   const dispatch = useDispatch();
+  const player = useSelector((state: RootState) => state.player);
+  // Phaser is used to create the game world where the player avatar will be displayed.
+  useEffect(() => {
+    const config: Phaser.Types.Core.GameConfig = {
+      type: Phaser.AUTO,
+      width: 800,
+      height: 600,
+      parent: 'phaser-avatar',
+      scene: {
+        preload: preload,
+        create: create,
+        update: update,
+      },
+      physics: {
+        default: 'arcade',
+        arcade: {
+          gravity: { y: 0 },
+          debug: false,
+        },
+      },
+    };
 
-  const handleMove = (direction: 'left' | 'right' | 'up' | 'down') => {
-    let newPosition = { ...player.position };
-    switch (direction) {
-      case 'left':
-        newPosition.x -= 10;
-        break;
-      case 'right':
-        newPosition.x += 10;
-        break;
-      case 'up':
-        newPosition.y -= 10;
-        break;
-      case 'down':
-        newPosition.y += 10;
-        break;
+    const game = new Phaser.Game(config);
+
+    function preload() {
+      this.load.spritesheet('player', 'assets/avatar.png', {
+        frameWidth: 32,
+        frameHeight: 48,
+      });
     }
-    dispatch(updatePosition(newPosition));
-  };
 
-  return (
-    <div>
-      <p>Player Position: X: {player.position.x}, Y: {player.position.y}</p>
-      <button onClick={() => handleMove('left')}>Move Left</button>
-      <button onClick={() => handleMove('right')}>Move Right</button>
-      <button onClick={() => handleMove('up')}>Move Up</button>
-      <button onClick={() => handleMove('down')}>Move Down</button>
-    </div>
-  );
+    function create() {
+      const playerSprite = new Player(this, player.x, player.y);
+      this.player = playerSprite;
+
+      // Create player animations
+      this.anims.create({
+        key: 'walk',
+        frames: this.anims.generateFrameNumbers('player', { start: 0, end: 3 }),
+        frameRate: 10,
+        repeat: -1,
+      });
+
+      this.player.sprite.play('walk');
+    }
+
+    function update() {
+      const cursors = this.input.keyboard.createCursorKeys();
+      const velocity = 160;
+
+      if (cursors.left.isDown) {
+        this.player.setVelocityX(-velocity);
+      } else if (cursors.right.isDown) {
+        this.player.setVelocityX(velocity);
+      } else {
+        this.player.setVelocityX(0);
+      }
+
+      if (cursors.up.isDown) {
+        this.player.setVelocityY(-velocity);
+      } else if (cursors.down.isDown) {
+        this.player.setVelocityY(velocity);
+      } else {
+        this.player.setVelocityY(0);
+      }
+
+      // Update player position in the Redux store
+      dispatch(updatePosition({ x: this.player.sprite.x, y: this.player.sprite.y }));
+    }
+
+    return () => {
+      game.destroy(true); // Clean up on unmount
+    };
+  }, [dispatch, player.x, player.y]);
+
+  return <div id="phaser-avatar"></div>;
 };
-
-export default Avatar;
--- frontend/src/phaser/Player.ts
+++ frontend/src/phaser/Player.ts
@@ -1,23 +1,20 @@
 import Phaser from 'phaser';
 
 export class Player {
-  constructor(scene, x, y) {
-    // Create a player sprite
+  sprite: Phaser.Physics.Arcade.Sprite;
+
+  constructor(scene: Phaser.Scene, x: number, y: number) {
     this.sprite = scene.physics.add.sprite(x, y, 'player');
-    
-    // Define player size and boundaries
-    this.sprite.setCollideWorldBounds(true);
+    this.sprite.setCollideWorldBounds(true); // Ensure the player stays within the game bounds
   }
 
-  update(time, delta) {
-    // Any player update logic can go here
-  }
-
-  setVelocityX(value) {
+  // Update the player's X velocity
+  setVelocityX(value: number) {
     this.sprite.setVelocityX(value);
   }
 
-  setVelocityY(value) {
+  // Update the player's Y velocity
+  setVelocityY(value: number) {
     this.sprite.setVelocityY(value);
   }
 }
